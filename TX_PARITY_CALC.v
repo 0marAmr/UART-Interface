0ed1ec38a09125b3aaeb7ed33faa318660d1b02c
Omar Amr <eng.omar.amr@gmail.com>
2023-08-20 22:42:44 +0300

    correct even parity and add par_en

--- TX_PARITY_CALC.v
+++ TX_PARITY_CALC.v
@@ -2,24 +2,24 @@ module TX_PARITY_CALC #(
     parameter DATA_WIDTH = 8
 )(
     input wire CLK, RST,
-    input wire Data_Valid,
+    input wire par_en,
     input wire PAR_TYP,
     input wire [DATA_WIDTH-1:0] P_DATA,
     output reg par_bit
 );
     
-    localparam  EVEN_PARITY  = 'b1;
+    localparam  EVEN_PARITY  = 'b0;
 
     always @(posedge CLK or negedge RST) begin
-        if (~RST) begin
-            par_bit <= 0;
+        if(~RST)begin
+            par_bit <= 1'b0;
         end
-        else if (Data_Valid) begin
+        if (par_en) begin
             if (PAR_TYP == EVEN_PARITY) begin
-                par_bit = ^P_DATA;
+                par_bit <= ^P_DATA;
             end
             else  begin /*Odd Parity*/
-                par_bit = ~^P_DATA;
+                par_bit <= ~^P_DATA;
             end
         end
     end
