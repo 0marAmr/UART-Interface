cab9e2d90f571a4ae4e98a720a812b9f2a089750
Omar Amr <eng.omar.amr@gmail.com>
2023-09-05 01:12:32 +0300

    Fix prescale

--- RX_DATA_SAMPLING.v
+++ RX_DATA_SAMPLING.v
@@ -2,14 +2,13 @@ module RX_DATA_SAMPLING (
     input wire          CLK,                 // RX Clock input signal
     input wire          RST,    
     input wire          RX_IN,
-    input wire  [4:0]   Prescale,
+    input wire  [5:0]   Prescale,
     input wire          data_samp_en,
     input wire  [4:0]   edge_cnt,
-    output reg          sampling_done,
     output reg          sampled_bit
 );
 
-    wire [3:0] half_prescale;
+    wire [4:0] half_prescale;
     assign half_prescale = Prescale >> 1;
 
     reg [2:0] samples;
@@ -31,7 +30,7 @@ module RX_DATA_SAMPLING (
     end
 
     always @(*) begin
-        if ((half_prescale == 4'd2) && (edge_cnt == 5'd2))begin
+        if ((half_prescale == 4'd2) && (edge_cnt == 5'd2)) begin
             sampled_bit = RX_IN;
         end
         else begin
@@ -39,15 +38,6 @@ module RX_DATA_SAMPLING (
                 3'b000, 3'b001, 3'b010, 3'b100: sampled_bit = 1'b0;
                 3'b011, 3'b101, 3'b110, 3'b111: sampled_bit = 1'b1;
             endcase
-        end
-    end
-    
-    always @(*) begin
-        if (half_prescale == 4'd2) begin
-            sampling_done = (edge_cnt == half_prescale);
-        end
-        else begin
-            sampling_done = (edge_cnt == (half_prescale + 4'b1));
         end
     end
 endmodule
